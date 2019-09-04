2937de342f5c732feb648e9e2b8ac388d4451fe9
Karl Nyrén <kallejohan211@hotmail.com>
2019-09-04 11:09:30 +0200

    idk, trying to specify directory

--- Scripts/02_tblastn_singlefile_nr.sh
+++ Scripts/02_tblastn_singlefile_nr.sh
@@ -3,7 +3,7 @@
 #SBATCH -A snic2018-3-568
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 01:30:00
+#SBATCH -t 00:10:00
 #SBATCH -J noniterative_tblastn_nt
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user karl.nyren.6523@student.uu.se
@@ -18,9 +18,5 @@ OUT_NAME='kirc_tblastn_020919'
 # Database used: nt - almost non-redundant database
 for file in $IN_FILE_PATH;
     do
-        command tblastn \
-        -db nt \
-        -query $file \
-        -outfmt 6 \
-        -out $OUT_FILE_PATH/$OUT_NAME
+        command tblastn -db /sw/data/uppnex/blast_databases/nt -query $file -outfmt 6 -out $OUT_FILE_PATH/$OUT_NAME
 done
