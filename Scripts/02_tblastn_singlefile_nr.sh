cc22dd6bbe1d877364732c5449f312e642f76145
Karl Nyrén <kallejohan211@hotmail.com>
2019-09-04 10:01:19 +0200

    increased time, messed around with in path

--- Scripts/02_tblastn_singlefile_nr.sh
+++ Scripts/02_tblastn_singlefile_nr.sh
@@ -3,7 +3,7 @@
 #SBATCH -A snic2018-3-568
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 00:30:00
+#SBATCH -t 01:30:00
 #SBATCH -J noniterative_tblastn_nt
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user karl.nyren.6523@student.uu.se
@@ -11,12 +11,12 @@
 module load bioinfo-tools
 module load blast/2.7.1+
 
-IN_FILE_PATH='/home/karlnyr/research_training_19/crypton_sequences'
+IN_FILE_PATH='/home/karlnyr/research_training_19/crypton_sequences/kirc.fasta'
 OUT_FILE_PATH='/home/karlnyr/research_training_19/blast_queries'
 OUT_NAME='kirc_tblastn_020919'
 
 # Database used: nt - almost non-redundant database
-for file in $IN_FILE_PATH/kirc*;
+for file in $IN_FILE_PATH;
     do
         command tblastn \
         -db nt \
