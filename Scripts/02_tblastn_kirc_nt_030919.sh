f7eda6555b53fb9a63da9a1e41ca4af9a15b2af8
Karl Nyrén <kallejohan211@hotmail.com>
2019-09-05 10:33:05 +0200

    added format options, changed to new fasta

--- Scripts/02_tblastn_kirc_nt_030919.sh
+++ Scripts/02_tblastn_kirc_nt_030919.sh
@@ -3,7 +3,7 @@
 #SBATCH -A snic2018-3-568
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 01:00:00
+#SBATCH -t 01:30:00
 #SBATCH -J noniterative_tblastn_nt
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user karl.nyren.6523@student.uu.se
@@ -11,16 +11,36 @@
 module load bioinfo-tools
 module load blast/2.7.1+
 
-IN_FILE_PATH='/home/karlnyr/research_training_19/crypton_sequences/consensus_sequences/kirc.fasta'
+IN_FILE_PATH='/home/karlnyr/research_training_19/crypton_sequences/consensus_sequences/Crypton_V_repbase090219.fasta'
 OUT_FILE_PATH='/home/karlnyr/research_training_19/blast_queries'
-OUT_NAME='kirc_tblastn_020919.xml'
+OUT_NAME='crypton_V_050919'
+XML_EXT='.xml'
+FMT_6_EXT='_fmt6'
 
 # Database used: nt - almost non-redundant database
-for file in $IN_FILE_PATH;
-    do
-        tblastn -db nt \
-        -query $file \
-        -outfmt 5 \
-        -num_threads 4 \
-        -out $OUT_FILE_PATH/$OUT_NAME
-done
+# $FMT Needs to be passed from sbatach format
+
+if [[ $FMT = '6' ]];
+    then
+    for file in $IN_FILE_PATH;
+        do
+            tblastn -db nt \
+            -query $file \
+            -outfmt $FMT \
+            -num_threads 4 \
+            -out $OUT_FILE_PATH/$OUT_NAME$FMT_6_EXT
+    done
+elif [[ $FMT = '5' ]];
+    then
+    for file in $IN_FILE_PATH;
+        do
+            tblastn -db nt \
+            -query $file \
+            -outfmt $FMT \
+            -num_threads 4 \
+            -out $OUT_FILE_PATH/$OUT_NAME$XML_EXT
+    done
+else
+    echo 'No valid outformat was provided, try --export=FMT=<format>'
+fi
+
