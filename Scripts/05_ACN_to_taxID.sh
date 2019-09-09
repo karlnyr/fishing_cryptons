350ac2745dcbb008e89fb38c217dea165ef62782
Karl Nyrén <kallejohan211@hotmail.com>
2019-09-09 09:39:00 +0200

    missed file spec in before wildcard symbol

--- Scripts/05_ACN_to_taxID.sh
+++ Scripts/05_ACN_to_taxID.sh
@@ -10,7 +10,7 @@
 
 ACC_NR_FILE=$1
 OUT_NAME=$2
-ACC_NR_2_TID_PATH='/sw/data/uppnex/ncbi_taxonomy/latest/accession2taxid/'*
+ACC_NR_2_TID_PATH='/sw/data/uppnex/ncbi_taxonomy/latest/accession2taxid/nucl_'*
 ID_TO_NAME_PATH='/sw/data/uppnex/ncbi_taxonomy/latest/names.dmp'
 TMP_file_PATH='/home/karlnyr'
 OUT_FILE_PATH='/home/karlnyr/research_trainging_19/blast_queries/genome_hits'
@@ -19,3 +19,4 @@ OUT_FILE_PATH='/home/karlnyr/research_trainging_19/blast_queries/genome_hits'
 fgrep -w -f $1 $ACC_NR_2_TID_PATH | awk 'F="\t" {print $3}' > $TMP_file_PATH/$OUT_NAME'_tmp.txt'
 fgrep -w -f $TMP_file_PATH/$OUT_NAME'_tmp.txt' $ID_TO_NAME_PATH | awk 'F="\t" {print $3}' > $OUT_FILE_PATH/$OUT_NAME
 rm $TMP_file_PATH/$OUT_NAME'_tmp.txt'
+
