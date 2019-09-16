1b0b03a34305060c55a85a0f9d9e6b741a7eefb6
Karl Nyrén <kallejohan211@hotmail.com>
2019-09-16 11:23:45 +0200

    nomen

--- Scripts/05_ACN_to_taxID.sh
+++ Scripts/05_ACN_to_taxID.sh
@@ -17,8 +17,8 @@ OUT_FILE_PATH='/home/karlnyr/research_training_19/blast_queries/genome_hits'
 PY_SCRIPT_PATH='/home/karlnyr/research_training_19/Scripts/06_filter_tid_count.py'
 A_T_EXT='_acn_tid'
 FILT_EXT='_f_tid'
-FILT_A_T='filt_acn_tid'
-FILT_BLAST='filtered_blast_hits'
+FILT_A_T='_filt_acn_tid'
+FILT_BLAST='_filtered_blast_hits'
 
 fgrep -w -f $1 $ACC_NR_2_TID_PATH | \
     awk -F "\t" '{print $2"\t"$3}' > $OUT_FILE_PATH/$OUT_NAME$A_T_EXT
