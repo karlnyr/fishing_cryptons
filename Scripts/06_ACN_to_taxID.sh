e68994d70499038fc5e76db1cc669195c677891a
Karl Nyrén <kallejohan211@hotmail.com>
2019-09-19 17:10:06 +0200

    minor sorting adjustments

--- Scripts/06_ACN_to_taxID.sh
+++ Scripts/06_ACN_to_taxID.sh
@@ -20,34 +20,34 @@ FILT_EXT='h'$HIT_PARAM'_f_tid'
 FILT_A_T='h'$HIT_PARAM'_filt_acn_tid'
 FILT_BLAST='h'$HIT_PARAM'_filtered_blast_hits'
 
-echo "Initiate ACN to TID search"
+echo "1 - Initiate ACN to TID search"
 join -t $'\t' \
     -1 2 -2 2 \
     -o 1.1,1.2,2.3 \
-    <(sort -t$'\t' -k2 $1) \
-    <(sort -t$'\t' -k2,2 $ACC_NR_2_TID_PATH) \
+    <(sort -t $'\t' -k2,2 $1) \
+    <(sort -t $'\t' -k2,2 $ACC_NR_2_TID_PATH) \
     > $OUT_FILE_PATH/$OUT_NAME$A_T_EXT
-echo "Done"
+echo "1 - Done"
 
-echo "Initiate blast hit filtering(python), fetching sequences with more than "$HIT_PARAM" hits to the same taxa"
+echo "2 - Initiate blast hit filtering(python), fetching sequences with more than "$HIT_PARAM" hits to the same taxa"
 python3 $PY_SCRIPT_PATH $OUT_FILE_PATH/$OUT_NAME$A_T_EXT $3 \
     > $OUT_FILE_PATH/$OUT_NAME$FILT_EXT
-echo "Done"
+echo "2 - Done"
 
-echo "Initiate blast hit filtering(shell), retri"
+echo "3 - Initiate blast hit filtering(shell), fetching these proteins from all sequences matched"
 join -t $'\t' \
     -1 2 -2 1 \
     -o 2.1,2.2,2.3 \
     <(sort -t $'\t' -k2 $OUT_FILE_PATH/$OUT_NAME$FILT_EXT) \
-    <(sort -k3 $OUT_FILE_PATH/$OUT_NAME$A_T_EXT) \
+    <(sort -t $'\t' -k1 $OUT_FILE_PATH/$OUT_NAME$A_T_EXT) \
     > $OUT_FILE_PATH/$OUT_NAME$FILT_A_T
-echo "Done"
+echo "3 - Done"
 
-echo "Initate final join on taxa id to taxa name"
+echo "4 - Initate final join on taxa id to taxa name"
 join -t $'\t' \
     -1 3 -2 1 \
     -o 1.1,1.2,1.3,1.4,2.2 \
     <(sort -t $'\t' -k3 $OUT_FILE_PATH/$OUT_NAME$FILT_A_T) \
     <(sort -t $'\t' -k1,1 $ID_TO_NAME_PATH) \
     > $OUT_FILE_PATH/$OUT_NAME$FILT_BLAST
-echo "Done"
+echo "4 - Done"
