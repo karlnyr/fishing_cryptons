b2fafd501ad21b0fe92f811cd67234c3606c8160
Karl Nyrén <kallejohan211@hotmail.com>
2019-09-19 15:27:17 +0200

    new time

--- Scripts/06_ACN_to_taxID.sh
+++ Scripts/06_ACN_to_taxID.sh
@@ -3,7 +3,7 @@
 #SBATCH -A snic2018-3-568
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 00:05:00
+#SBATCH -t 00:30:00
 #SBATCH -J Extracting_Taxa_name_from_ACN
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user karl.nyren.6523@student.uu.se
