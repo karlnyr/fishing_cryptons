7ad14829f0208a78e47c23338477bc4a43735d74
Karl Nyrén <kallejohan211@hotmail.com>
2019-09-20 16:01:14 +0200

    rename

--- scripts/07_extract_novel_taxa.py
+++ scripts/07_extract_novel_taxa.py
@@ -10,11 +10,12 @@ with open(sys.argv[1]) as rep_f, \
     # For some reason the format is wonky on the join, here fixing that here
     for line in blast_h:
         bh.append([f"{line.split()[0]}", f"{line.split()[1]}", f"{line.split()[2]} {line.split()[3]}"])
-    hit_count = len(rb)
+    entries = len(rb)
+    # Very uncool loop but it works.
     for filt_hit in bh:
         loop_c = 0
         flag = 0
-        while flag == 0 and loop_c <= hit_count:
+        while flag == 0 and loop_c <= entries:
             for row in rb:
                 if filt_hit[2] == row['discovered in'] and filt_hit[0]:
                     print(f"{filt_hit[2]} = {species}")
@@ -23,8 +24,4 @@ with open(sys.argv[1]) as rep_f, \
                 elif filt_hit[2] != species:
                     print(f"{filt_hit[2]} != {species}")
                     loop_c += 1
-
-            # if filt_hit[2] not in rb['discovered_in']:
-            #     pass
-            # elif filt_hit[2] in rb['discovered_in']:
-            #     print('already found')
+        if flag == 0:
