d8b0668da90eb48f7be08345e413b9530b3971cc
Karl Nyrén <kallejohan211@hotmail.com>
2019-09-30 18:01:21 +0200

    new acn_script, adapted to filter script

--- scripts/06_ACN_to_taxID.sh
+++ scripts/06_ACN_to_taxID.sh
@@ -40,34 +40,25 @@ python3 $PY_05_PATH $OUT_FILE_PATH/$OUT_NAME$A_T_EXT $4 \
     > $OUT_FILE_PATH/$OUT_NAME$FILT_EXT
 echo "2 - Done"
 
-echo "3 - Initiate blast hit filtering(shell), fetching these proteins from all sequences matched"
-join -t $'\t' \
-    -1 2 -2 1 \
-    -o 2.1,2.2,2.3 \
-    <(sort -t $'\t' -k2,2 $OUT_FILE_PATH/$OUT_NAME$FILT_EXT) \
-    <(sort -t $'\t' -k1,1 $OUT_FILE_PATH/$OUT_NAME$A_T_EXT) \
-    > $OUT_FILE_PATH/$OUT_NAME$FILT_A_T
-echo "3 - Done"
-
-echo "4 - Initate final join on taxa id to taxa name"
+echo "3 - Initate final join on taxa id to taxa name"
 join -t $'\t' \
     -1 3 -2 1 \
     -o 1.1,1.2,1.3,2.4 \
-    <(sort -t $'\t' -k3,3 $OUT_FILE_PATH/$OUT_NAME$FILT_A_T) \
+    <(sort -t $'\t' -k3,3 $OUT_FILE_PATH/$OUT_NAME$FILT_EXT) \
     <(sort -t $'\t' -k1,1 $ID_TO_NAME_PATH) \
     > $OUT_FILE_PATH/$OUT_NAME$FILT_BLAST
-echo "4 - Done"
+echo "3 - Done"
 
-echo "5 - Filtering out unique protein and taxa combinations"
+echo "4 - Filtering out unique protein and taxa combinations"
 cut -f1,2,4 $OUT_FILE_PATH/$OUT_NAME$FILT_BLAST \
     | uniq \
     | sort -k1 > $OUT_FILE_PATH/$OUT_NAME$UINIQ_COMB
-echo "5 - Done"
+echo "4 - Done"
 
-echo "6 - Extracting format 6 information of filtered proteins"
+echo "5 - Extracting format 6 information of filtered proteins"
 python3 $PY_07_PATH \
     $REPBASE \
     $OUT_FILE_PATH/$OUT_NAME$UINIQ_COMB \
     $4 \
     $OUT_FILE_PATH/$OUT_NAME
-echo "6 - Done"
+echo "5 - Done"
