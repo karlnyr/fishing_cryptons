173239570549721dda2fc135150eef6aacc1df94
kethuth <kallejohan211@hotmail.com>
2019-10-22 22:36:44 +0200

    pathing

--- scripts/10_alignment_clustering.sh
+++ scripts/10_alignment_clustering.sh
@@ -11,12 +11,12 @@ module load biopython/1.73-py3
 module load blast/2.9.0+
 
 SCRIPT_PATH='/home/karlnyr/research_training_19/scripts/09_alignment_grouping.py'
-FILE_PATH='/home/karlnyr/research_training_19/alignments/*/02/*'
+FILE_PATH='/home/karlnyr/research_training_19/alignments/*/02*/*'
 PERC_CUTOF=$2
 MIN_LENGTH=$3
 
 for file in $FILE_PATH;
     do
         python3 $SCRIPT_PATH $file $PERC_CUTOF $MIN_LENGTH \
-        > $file'_clustering'
+	> $file'_clustering'
 done
