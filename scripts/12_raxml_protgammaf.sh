54f160ece8ab5fdc2f077bff77923ed376c2b9af
kethuth <kallejohan211@hotmail.com>
2019-11-01 10:16:29 +0100

    phylogeny tree added

--- scripts/12_raxml_protgammaf.sh
+++ scripts/12_raxml_protgammaf.sh
@@ -13,14 +13,16 @@ module load raxml/8.2.10-gcc-mpi
 
 ALIGNMENT_FILE_PATH=$1
 OUTFILE_PATH=$2
-MODEL='PROTGAMMAWGAF'
+MODEL='PROTGAMMAWAGF'
 
 
-# MPI loaded for repetitive
+# MPI loaded for boostrapping, T for number of threads(1 thread per core recommended), -b for random bootstrap seed(reproducible if same data), 
+# -B for branch resolution, -N Number of bootstraps to be performed, -p to reproduce ML.
 raxmlHPC -s $ALIGNMENT_FILE_PATH \
     -n $OUTFILE_PATH \
     -m $MODEL \
     -T 6 \
     -b 126345 \
     -B 0.03 \
-    -N 10
+    -N 150 \
+    -p 3214
