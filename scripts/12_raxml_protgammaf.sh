fd84ee14d8b8d783402599f4962346a1dfc9587b
Karl Nyrén <kallejohan211@hotmail.com>
2019-11-02 16:50:13 +0100

    new raxmlscript, creates consensus and draws out the bipartitions

--- scripts/12_raxml_protgammaf.sh
+++ scripts/12_raxml_protgammaf.sh
@@ -12,18 +12,36 @@ module load bioinfo-tools
 module load raxml/8.2.10-gcc-mpi
 
 ALIGNMENT_FILE_PATH=$1
-OUTFILE_PATH=$2
+FILE_EXT=$2
+MR_CONS='MR_CONS'
 MODEL='PROTGAMMAWAGF'
-
+BS_EXT='RAxML_bootstrap.'
+mr_cons_BS='MR_CONS-bootstrap'
 
 # MPI loaded for boostrapping, T for number of threads(1 thread per core recommended), -b for random bootstrap seed(reproducible if same data),
 # -B for branch resolution, -N Number of bootstraps to be performed, -p to reproduce ML.
-raxmlHPC -s $ALIGNMENT_FILE_PATH \
-    -f b \
-    -n $OUTFILE_PATH \
+raxmlHPC \
+    -s $ALIGNMENT_FILE_PATH \
+    -n $FILE_EXT \
     -m $MODEL \
     -T 12 \
     -b 126345 \
     -B 0.03 \
     -N 150 \
     -p 3214
+
+
+raxmlHPC \
+    -I autoMRE \
+    -m PROTGAMMAWAGF \
+    -z $BS_EXT$OUTFILE_PATH \
+    -n $MR_CONS \
+    -T 12 \
+    -p 12321341
+
+raxmlHPC \
+    -f b \
+    -z $BS_EXT$OUTFILE_PATH \
+    -t $BS_EXT$OUTFILE_PATH$MR_CONS \
+    -m PROTGAMMAWAGF \
+    -n $mr_cons_BS
