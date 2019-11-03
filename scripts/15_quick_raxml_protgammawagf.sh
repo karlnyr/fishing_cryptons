c55dfb655f91470ce338fbc2514205201dd51338
Karl Nyrén <kallejohan211@hotmail.com>
2019-11-03 11:43:49 +0100

    fast raxml fixed

--- scripts/15_quick_raxml_protgammawagf.sh
+++ scripts/15_quick_raxml_protgammawagf.sh
@@ -19,11 +19,11 @@ MODEL='PROTGAMMAWAGF'
 # MPI loaded for boostrapping, T for number of threads(1 thread per core recommended), -b for random bootstrap seed(reproducible if same data),
 # -N Number of bootstraps to be performed, -p to reproduce ML.
 raxmlHPC \
-    -f a
+    -f a \
+    -p 3214 \
+    -x 123123 \
     -s $ALIGNMENT_FILE_PATH \
     -n $FILE_EXT \
     -m $MODEL \
     -T 12 \
-    -p 3214 \
-    -N autoMRE \
-    -x 123123
+    -N autoMRE
