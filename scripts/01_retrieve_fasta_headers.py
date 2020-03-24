52f418f85b9e320704d308cb6a9ea333b687bc5b
Karl Nyrén <45558267+karlnyr@users.noreply.github.com>
2020-03-24 10:53:57 +0100

    Update 01_retrieve_fasta_headers.py

--- scripts/01_retrieve_fasta_headers.py
+++ scripts/01_retrieve_fasta_headers.py
@@ -2,10 +2,10 @@ import sys
 import csv
 from Bio import SeqIO
 
+'''put fasta headers into separate csv file. 
+01_retrieve_fasta_headers.py <fasta> <out_prefix>'''
 
 def extract_fasta_headers_to_csv():
-    '''put fasta headers into separate csv file. First arg is fasta to extract,
-    Second arg is outfile with .csv extension'''
     with open(sys.argv[2], 'w') as csvf:
         filewriter = csv.writer(csvf, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         with open(sys.argv[1], "rU") as handle:
